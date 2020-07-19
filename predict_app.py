bb6fc60b18351014621dd74294e41b920ab822bd
Siddharth Dhote <41946313+siddharthdhote@users.noreply.github.com>
2020-07-19 10:14:46 +0530

    Update predict_app.py

--- predict_app.py
+++ predict_app.py
@@ -39,7 +39,7 @@ def preprocess_image(image,target_size):
 print("* Loading Keras model...")
 get_model()
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["POST"])
 def predict():
     message=request.get_json(force=True)
     encoded=message['image']
